9221bc04f9a50e7c2f5f5a84c1e2f7910d61a8f2
Eric Sandström <eric.wiljam.sandstrom@gmail.com>
2018-03-19 06:13:32 +0100

    project-footr init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,22 @@
 import { Component } from '@angular/core';
-
+import { AngularFirestore } from 'angularfire2/firestore';
+import { AngularFireAuth } from 'angularfire2/auth';
+import { Observable } from 'rxjs/Observable';
+import * as firebase from 'firebase/app';
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'app';
+    items: Observable<any[]>;
+    constructor(db: AngularFirestore, public afAuth: AngularFireAuth) {
+        this.items = db.collection('items').valueChanges();
+    }
+    login() {
+        this.afAuth.auth.signInWithPopup(new firebase.auth.GoogleAuthProvider());
+    }
+    logout() {
+        this.afAuth.auth.signOut();
+    }
 }
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,5 +4,13 @@
 // The list of which env maps to which file can be found in `.angular-cli.json`.
 
 export const environment = {
-  production: false
+    production: false,
+    firebase: {
+        apiKey: "AIzaSyB7rVBm75jmINrqg7XyCqida0Ibm1Fohs8",
+        authDomain: "project-footr.firebaseapp.com",
+        databaseURL: "https://project-footr.firebaseio.com",
+        projectId: "project-footr",
+        storageBucket: "project-footr.appspot.com",
+        messagingSenderId: "568588022246"
+    }
 };
